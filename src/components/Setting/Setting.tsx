1dafa0a4d5c5c0640ec766146f06694204f23fbc
leejehoon92 <2eejehoon@gmail.com>
2023-03-04 20:35:53 +0900

    style: Setting 컴포넌트 스타일링
    1. 조건부 렌더링
    - '설정' 버튼을 누르면 CustomSetting, DifficultySetting 컴포넌트를 렌더링
    
    2. position: fixed
    - Setting 컴포넌트를 브라우저 우측 하단에 고정

--- src/components/Setting/Setting.tsx
+++ src/components/Setting/Setting.tsx
@@ -1,13 +1,18 @@
-import { memo } from "react";
-import { Wrapper } from "./SettingStyle";
+import { memo, useCallback, useState } from "react";
+import { Wrapper, StyledButton } from "./SettingStyle";
 import CustomSetting from "../CustomSetting/CustomSetting";
 import DifficultySetting from "../DifficultySetting/DifficultySetting";
 
 function Setting() {
+  const [isSetting, setIsSetting] = useState(false);
+
+  const handleClick = useCallback(() => setIsSetting((prev) => !prev), []);
+
   return (
     <Wrapper>
-      <CustomSetting />
-      <DifficultySetting />
+      <StyledButton onClick={handleClick}>설정</StyledButton>
+      {isSetting && <CustomSetting />}
+      {isSetting && <DifficultySetting />}
     </Wrapper>
   );
 }
--- src/components/Setting/SettingStyle.ts
+++ src/components/Setting/SettingStyle.ts
@@ -1,12 +1,29 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-  top: 10px;
-  left: 5px;
+  bottom: 100px;
+  right: 5px;
   position: fixed;
   width: 260px;
   height: 200px;
   display: flex;
   justify-content: space-evenly;
   align-items: center;
+`;
+
+export const StyledButton = styled.button`
+  bottom: 20px;
+  right: 20px;
+  position: fixed;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 60px;
+  height: 60px;
+  border: none;
+  border-radius: 50%;
+  background-color: tomato;
+  color: white;
+  font-size: 20px;
+  font-weight: 600;
 `;
